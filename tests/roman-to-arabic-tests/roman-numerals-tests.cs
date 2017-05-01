9c6fc8e03546466ef5badc55843c63cdb1b2b341
Douglas Waugh <douglas.waugh.e3@gmail.com>
2017-05-01 18:55:54 +0100

    Rename test class

--- tests/roman-to-arabic-tests/roman-numerals-tests.cs
+++ tests/roman-to-arabic-tests/roman-numerals-tests.cs
@@ -4,7 +4,7 @@ using RomanNumeralsLibrary;
 
 namespace roman_to_arabic_tests
 {
-    public class UnitTest1
+    public class RomanNumeralsTests
     {
         [Theory]
         [InlineData("", 0)]
